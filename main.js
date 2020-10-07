9f4dfcc9a1559548f2c93e82a88767ce834e690f
Jessica Michel <>
2020-10-07 17:10:30 +0200

    added comments

--- main.js
+++ main.js
@@ -1,28 +1,9 @@
-        // Stap 4
-        console.log("Hello Winc Academy!");
+     console.log("Hallo WInc Academy studenten");
 
-        // Stap 5
-        let name = 'Jessica';
-        console.log(name);
+     /* let multiple = 1230941 * 1823794;
+     console.log(multiple);
 
-        // Stap 6
-        // Antwoord: Er kunnen geen variable gemaakt worden met JS conditional statements
+     let divide = 637263 / 54;
+     console.log(divide); */
 
-        // Stap 7
-        console.log(4 + 4);
-        console.log('4' + '4'); // Geeft alleen beide strings weer
-
-        // Stap 8
-        console.log(286 - 80);
-        console.log(738 + 5);
-        console.log(26 * 37);
-        console.log(50 / 5);
-        console.log(20 % 6);
-
-        // Stap 9
-        let age = 27;
-        console.log(typeof age);
-
-        // Stap 10
-        age = 'zevenentwintig';
-        console.log(typeof age);
+     // Dit is een grote som, comments worden niet weergegeven in de console of terminal
