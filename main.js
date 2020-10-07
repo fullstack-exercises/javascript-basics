6fb90156efdd1aa5a4f719b7a18f93489c871df3
Jessica Michel <>
2020-10-07 15:49:38 +0200

    First Javascript console logs

---
+++ main.js
@@ -0,0 +1,28 @@
+        // Stap 4
+        console.log("Hello Winc Academy!");
+
+        // Stap 5
+        let name = 'Jessica';
+        console.log(name);
+
+        // Stap 6
+        // Antwoord: Er kunnen geen variable gemaakt worden met JS conditional statements
+
+        // Stap 7
+        console.log(4 + 4);
+        console.log('4' + '4'); // Geeft alleen beide strings weer
+
+        // Stap 8
+        console.log(286 - 80);
+        console.log(738 + 5);
+        console.log(26 * 37);
+        console.log(50 / 5);
+        console.log(20 % 6);
+
+        // Stap 9
+        let age = 27;
+        console.log(typeof age);
+
+        // Stap 10
+        age = 'zevenentwintig';
+        console.log(typeof age);
